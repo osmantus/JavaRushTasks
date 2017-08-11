107ade1abafad4581c1647988c435d56a8ed7a0d
Alex <poznanski.alex@gmail.com>
2017-08-11 17:39:59 +0300

    commit after merge of two projects

--- 4.JavaCollections/src/com/javarush/task/task33/task3304/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3304/Solution.java
@@ -1,8 +1,8 @@
 package com.javarush.task.task33.task3304;
 
-/*import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;*/
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -21,15 +21,15 @@ public class Solution {
         return null;
     }
 
-    /*@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
-    @JsonSubTypes(@JsonSubTypes.Type(value=First.class,  name="first"))*/
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=First.class,  name="first"))
     public static class First {
         public int i;
         public String name;
     }
 
-    /*@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
-    @JsonSubTypes(@JsonSubTypes.Type(value=Second.class, name="second"))*/
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=Second.class, name="second"))
     public static class Second {
         public int i;
         public String name;
