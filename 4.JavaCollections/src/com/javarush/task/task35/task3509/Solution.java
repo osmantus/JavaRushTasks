1648713564bbc201d5fd8532d19fb8817f85b953
Alex <poznanski.alex@gmail.com>
2017-08-15 21:47:52 +0300

    commit 4

--- 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -9,26 +9,53 @@ Collections & Generics
 public class Solution {
 
     public static void main(String[] args) {
+        List<Integer> list0 = newArrayList(new Integer(1), new Integer(3));
+        newHashSet(new Integer(1), new Integer(3));
+        List<String> list1 = new ArrayList<String>();
+        list1.add("1");
+        List<Integer> list2 = new ArrayList<Integer>();
+        list2.add(5);
+        newHashMap(list1, list2);
     }
 
-    public static <T> ArrayList newArrayList(Object... elements) {
+    public static <T> ArrayList<T> newArrayList(T... elements) {
         //напишите тут ваш код
         ArrayList<T> list = new ArrayList<>();
+        if (elements.length > 0)
+        {
+            for (Object o : elements) {
+                list.add((T) o);
+            }
+        }
         return list;
     }
 
-    public static <T> HashSet newHashSet(Object... elements) {
+    public static <T> HashSet<T> newHashSet(T... elements) {
         //напишите тут ваш код
         HashSet<T> set = new HashSet<>();
+        if (elements.length > 0)
+        {
+            for (Object o : elements) {
+                set.add((T) o);
+            }
+        }
+
         return set;
     }
 
-    public static <K, V> HashMap newHashMap(List<? extends K> keys, List<? extends V> values) {
+    public static <K, V> HashMap<K, V> newHashMap(List<? extends K> keys, List<? extends V> values) {
         //напишите тут ваш код
 
         if (keys.size() == values.size())
         {
             HashMap<K, V> map = new HashMap<>();
+
+            if (keys.size() > 0)
+            {
+                for (int i = 0; i < keys.size(); i++) {
+                    map.put(keys.get(i), values.get(i));
+                }
+            }
             return map;
         }
         else {
