24d689df9171a953666e1011e3e54b00d5410917
Alex <poznanski.alex@gmail.com>
2017-08-19 13:12:39 +0300

    commit 6

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task35.task3507;
 
+import java.io.File;
+import java.util.HashSet;
 import java.util.Set;
 
 /* 
@@ -12,6 +14,21 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
+
+        Set<? extends Animal> resultingSet = new HashSet<>();
+
+        File folder = new File(pathToAnimals);
+        File[] listOfFiles = folder.listFiles();
+        Class<? extends Animal> classObj = null;
+
+        //Reflections reflections = new Reflections(pathToAnimals);
+
+        /*for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                classObj = ClassLoader.
+            }
+        }*/
+
         return null;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task37.task3707;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -8,40 +8,129 @@ import java.util.*;
  */
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E>
 {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
     public AmigoSet()
     {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection)
+    {
+        map = new HashMap<>(Math.max(16, (int) (collection.size() / .75f + 1)));
+        this.addAll(collection);
     }
 
     @Override
-    public Spliterator spliterator()
-    {
-        return null;
+    public Object clone() {
+        try
+        {
+            AmigoSet set = (AmigoSet) super.clone();
+            set.map = (HashMap) map.clone();
+            return set;
+        }
+        catch (Exception e)
+        {
+            throw new InternalError();
+        }
     }
 
     @Override
-    public Iterator iterator()
+    public boolean add(E e) {
+        return null == map.put(e, PRESENT);
+    }
+
+    @Override
+    public Iterator<E> iterator()
     {
-        return null;
+        Set<E> set = map.keySet();
+        return set.iterator();
     }
 
     @Override
     public int size()
     {
-        return 0;
+        return map.size();
     }
 
     @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
+    public boolean isEmpty() {
+        return map.isEmpty();
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public boolean contains(Object o) {
+        return map.containsKey(o);
     }
 
     @Override
-    public boolean removeAll(Collection<?> c) {
-        return super.removeAll(c);
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == null;
+    }
+
+    private void writeObject(ObjectOutputStream outputStream) throws IOException
+    {
+        outputStream.defaultWriteObject();
+        Set keys = (Set) map.keySet();
+        //outputStream.writeObject(map.keySet());
+        outputStream.writeInt(map.size());
+        for (Object key : keys) {
+            outputStream.writeObject((E) key);
+        }
+
+        outputStream.writeInt(HashMapReflectionHelper.callHiddenMethod(map, "capacity"));
+        outputStream.writeFloat(HashMapReflectionHelper.callHiddenMethod(map, "loadFactor"));
+    }
+
+    private void readObject(ObjectInputStream inputStream) throws IOException, ClassNotFoundException
+    {
+        inputStream.defaultReadObject();
+        int mapSize = inputStream.readInt();
+
+        //Set keys = (Set) inputStream.readObject();
+        Set keys = new HashSet<E>();
+        Object key1 = null;
+        for (int i = 0; i < mapSize; i++) {
+            key1 = (E) inputStream.readObject();
+            keys.add(key1);
+        }
+
+        int capacity = inputStream.readInt();
+        float loadFactor = inputStream.readFloat();
+        map = new HashMap<E, Object>(capacity, loadFactor);
+        for (Object key2 : keys) {
+            map.put((E) key2, null);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        AmigoSet<Integer> amigoSet = new AmigoSet<>();
+        amigoSet.add(1);
+        amigoSet.add(5);
+
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("D:\\Work\\Test\\amigoset.txt"));
+
+            objectOutputStream.writeObject(amigoSet);
+            objectOutputStream.close();
+            //amigoSet.writeObject();
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("D:\\Work\\Test\\amigoset.txt"));
+            AmigoSet<Integer> amigoSet2 = (AmigoSet<Integer>) objectInputStream.readObject();
+            objectInputStream.close();
+            System.out.println();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
